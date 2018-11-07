8c8dbb3d832e876bd32fba9e62b447005751567b
Hector <hector.acosta.pozo@outlook.com>
2018-11-07 13:14:41 -0400

    Added missing

---
+++ honey-bits-frontend-android/app/src/main/java/com/teko/honeybits/honeybits/models/Avatar.java
@@ -0,0 +1,13 @@
+package com.teko.honeybits.honeybits.models;
+
+public class Avatar {
+    private String url = "";
+
+    public Avatar(String url) {
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+}
